81ff85e32bf70beca9898f1bb062ffa0fb713537
Andrew Jeremy <Andrewjeremy12345@gmail.com>
2022-03-25 03:22:12 +0700

    added interactive search bar

--- app/pages/SearchBar.tsx
+++ app/pages/SearchBar.tsx
@@ -17,14 +17,18 @@ export default function SearchBar({ filterText, onFilterTextChange }) {
             <h1 className="font-medium font-sans subpixel-antialiased text-xl">Search for a meme</h1>
           </div>
           <div className="w-full" />
-          <form className="w-full flex justify-center outline outline-2 outline-searchbar-outline rounded-xl pl-3 pr-3">
-            <img
-              alt="search"
-              src="/search.png"
-              className="w-2/7 rounded-tl-xl rounded-bl-xl p-2 w-10 bg-first object-cover"
-            />
+          <form className="group w-full flex justify-center outline outline-2 outline-searchbar-outline rounded-xl pl-3 pr-3">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="group-hover:stroke-fourth stroke-fourth/10 w-2/7 rounded-tl-xl rounded-bl-xl p-2 w-10 object-cover"
+              fill="none" viewBox="0 0 22 22"
+              stroke="currentColor"
+              stroke-width="2"
+            >
+              <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+            </svg>
             <input
-              className="w-full p-2 rounded-tr-xl rounded-br-xl bg-first border-solid"
+              className="w-full p-2 rounded-tr-xl rounded-br-xl bg-first border-solid placeholder:text-fourth/10 group-hover:placeholder:text-fourth"
               type="text"
               value={ filterText }
               placeholder="or die"
