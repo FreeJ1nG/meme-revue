d7ad9f5df12481b0c1df3bc67fd476f952028458
Andrew Jeremy <Andrewjeremy12345@gmail.com>
2022-03-25 03:28:29 +0700

    changed stroke attributes to camelCase

--- app/pages/SearchBar.tsx
+++ app/pages/SearchBar.tsx
@@ -23,9 +23,9 @@ export default function SearchBar({ filterText, onFilterTextChange }) {
               className="group-hover:stroke-fourth stroke-fourth/10 w-2/7 rounded-tl-xl rounded-bl-xl p-2 w-10 object-cover"
               fill="none" viewBox="0 0 22 22"
               stroke="currentColor"
-              stroke-width="2"
+              strokeWidth="2"
             >
-              <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+              <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
             </svg>
             <input
               className="w-full p-2 rounded-tr-xl rounded-br-xl bg-first border-solid placeholder:text-fourth/10 group-hover:placeholder:text-fourth"
