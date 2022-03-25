2b2b1978453dcd16de74d59131d1582c052637f0
Andrew Jeremy <Andrewjeremy12345@gmail.com>
2022-03-26 04:41:46 +0700

    added animations for Footer and searchandsavebar

--- app/pages/Footer.tsx
+++ app/pages/Footer.tsx
@@ -1,117 +1,70 @@
-import CompfestLogo from './CompfestLogo'
+import CompfestLogo from "./CompfestLogo"
 
 export default function Footer({ showFooter, handleClick }) {
   return (
     <div>
-    {
-      showFooter &&
-      <div className="group fixed inset-x-0 bottom-0 h-40 w-full bg-[#2D2F45]">
-        <button
-          onClick={ () => handleClick() }
-          className="absolute right-4 top-4"
-        >
+      <div
+        className={`fixed inset-x-0 bottom-0 w-full bg-[#2D2F45] transition-all flex w-full flex-wrap xl:flex-nowrap ${
+          showFooter && "h-[220px]"
+        } ${!showFooter && "h-12"}`}
+      >
+        <button onClick={() => handleClick()} className="">
           <img
-            src="/dropdown.png"
-            alt="dropdown"
-            className="w-10 animate-bounce"
+            src={showFooter ? "/dropdown.png" : "/reveal.png"}
+            alt="arrow"
+            className="w-10 animate-bounce absolute top-2 left-0 right-0 ml-auto mr-auto"
           />
         </button>
-        <div className="flex w-full h-full flex-wrap xl:flex-nowrap">
-          <div className="flex items-center xl:w-1/2 xl:h-full xl:justify-start justify-center w-full h-2/3">
-            <div className="relative xl:left-20">
-              <div className="opacity-80 w-full mb-4 flex justify-center xl:flex-none xl:justify-start">
-                <CompfestLogo />
-              </div>
-              <span className="font-inter font-medium text-white/80">&copy; 2022 COMPFEST. All rights reserved</span>
+        <div className="overflow-none flex items-center xl:w-1/2 xl:h-full xl:justify-start justify-center w-full h-2/3">
+          <div className="relative xl:left-20">
+            <div className="opacity-80 w-full mb-4 flex justify-center xl:flex-none xl:justify-start">
+              {showFooter && <CompfestLogo />}
             </div>
-          </div>
-          <div className="flex xl:flex-row-reverse xl:justify-start xl:h-full xl:w-1/2 justify-center items-center justify-start h-1/3 w-full">
-            <a
-              href="https://medium.com/compfest"
-              className="ml-8 xl:mr-16"
-            >
-              <img
-                src="/medium.png"
-                alt="medium"
-                className="w-6 h-[13.5px]"
-              />
-            </a>
-            <a
-              href="https://line.me/R/ti/p/%40compfest"
-              className="ml-8"
-            >
-              <img
-                src="/line.png"
-                alt="line"
-                className="w-5 h-[19px]"
-              />
-            </a>
-            <a
-              href="https://www.linkedin.com/company/compfest/"
-              className="ml-8"
-            >
-              <img
-                src="/linkedin.png"
-                alt="linkedin"
-                className="w-6 h-6"
-              />
-            </a>
-            <a
-              href="https://www.instagram.com/compfest/"
-              className="ml-8"
-            >
-              <img
-                src="/instagram.png"
-                alt="instagram"
-                className="w-6 h-6"
-              />
-            </a>
-            <a
-              href="https://www.youtube.com/channel/UC03QmhwvQEWVI608A-RCEpw/"
-              className="ml-8"
-            >
-              <img
-                src="/youtube.png"
-                alt="youtube"
-                className="w-5 h-4"
-              />
-            </a>
-            <a
-              href="https://twitter.com/compfest"
-              className="ml-8"
-            >
-              <img
-                src="/twitter.png"
-                alt="twitter"
-                className="w-5 h-[18px]"
-              />
-            </a>
-            <a
-              href="https://www.facebook.com/COMPFEST/"
-              className="ml-8"
-            >
-              <img
-                src="/facebook.png"
-                alt="facebook"
-                className="w-6 h-6"
-              />
-            </a>
+            {showFooter && (
+              <span className="font-inter font-medium text-white/80">
+                &copy; 2022 COMPFEST. All rights reserved
+              </span>
+            )}
           </div>
         </div>
+        <div className="flex xl:flex-row-reverse xl:justify-start xl:h-full xl:w-1/2 justify-center items-center justify-start h-1/3 w-full">
+          {showFooter && (
+            <a href="https://medium.com/compfest" className="ml-8 xl:mr-14">
+              <img src="/medium.png" alt="medium" className="w-6 h-[13.5px]" />
+            </a>
+          )}
+          {showFooter && (
+            <a href="https://line.me/R/ti/p/%40compfest" className="ml-8">
+              <img src="/line.png" alt="line" className="w-5 h-[19px]" />
+            </a>
+          )}
+          {showFooter && (
+            <a href="https://www.linkedin.com/company/compfest/" className="ml-8">
+              <img src="/linkedin.png" alt="linkedin" className="w-6 h-6" />
+            </a>
+          )}
+          {showFooter && (
+            <a href="https://www.instagram.com/compfest/" className="ml-8">
+              <img src="/instagram.png" alt="instagram" className="w-6 h-6" />
+            </a>
+          )}
+          {showFooter && (
+            <a href="https://www.youtube.com/channel/UC03QmhwvQEWVI608A-RCEpw/" className="ml-8">
+              <img src="/youtube.png" alt="youtube" className="w-5 h-4" />
+            </a>
+          )}
+          {showFooter && (
+            <a href="https://twitter.com/compfest" className="ml-8">
+              <img src="/twitter.png" alt="twitter" className="w-5 h-[18px]" />
+            </a>
+          )}
+          {showFooter && (
+            <a href="https://www.facebook.com/COMPFEST/" className="ml-8">
+              <img src="/facebook.png" alt="facebook" className="w-6 h-6" />
+            </a>
+          )}
+        </div>
       </div>
-    }
-    {
-      !showFooter &&
-      <div className="fixed inset-x-0 bottom-0 h-14 w-full bg-[#2D2F45]">
-        <button onClick={ () => handleClick() }>
-          <img
-            src="/reveal.png"
-            alt="reveal"
-            className="w-10 absolute right-4 bottom-0 animate-bounce"
-          />
-        </button>
-      </div>
-    }
     </div>
-  );
+  )
 }
--- app/pages/SearchAndSavedBar.tsx
+++ app/pages/SearchAndSavedBar.tsx
@@ -1,47 +1,42 @@
 import { useMeme } from "app/core/hooks/useMeme"
-import { useState } from 'react'
+import { useState } from "react"
 import { BlitzPage, Image } from "blitz"
-import Masonry from 'react-masonry-css'
-import SaveButton from './SaveButton'
-import SearchBar from './SearchBar'
-import MemeBox from './MemeBox'
-import CompfestLogo from './CompfestLogo'
-import MemeExploreBox from './MemeExploreBox'
+import Masonry from "react-masonry-css"
+import SaveButton from "./SaveButton"
+import SearchBar from "./SearchBar"
+import MemeBox from "./MemeBox"
+import CompfestLogo from "./CompfestLogo"
+import MemeExploreBox from "./MemeExploreBox"
 
 export default function SearchAndSavedBar({ search, handleClick }) {
-  const active = " bg-gradient-to-t from-third to-second border-4 border-white/10 rounded-xl ";
   return (
     <div className="flex justify-center items-center">
-      <div className="rounded-xl bg-black/10 flex justify-center xl:w-1/3 lg:w-2/5 m:w-1/2 sm:w-3/4">
+      <div className="relative h-12 rounded-xl bg-black/10 flex justify-center xl:w-1/3 lg:w-2/5 md:w-1/2 sm:w-3/4">
+        <div className="absolute flex top-0 bottom-0 left-0 right-0">
+          <div className={(search ? "w-0" : "w-1/2") + " duration-500 transition-all"}></div>
+          <div className="h-12 w-1/2 bg-gradient-to-t from-third to-second border-4 border-white/10 rounded-xl"></div>
+        </div>
         <button
-          className={ (search && active) + " w-1/2 py-3 px-16" }
-          onClick={ () => handleClick(true) }
+          className="group absolute left-0 w-1/2 py-3 px-16"
+          onClick={() => handleClick(true)}
         >
+          <div className="absolute top-0 left-0 right-0 bottom-0 group-hover:bg-white/10 rounded-xl"></div>
           <div className="flex justify-center items-center">
-            <img
-              src="/search-button.png"
-              className="w-5 h-5 mr-4"
-            />
-            <span className="font-semibold font-inter">
-              Explore
-            </span>
+            <img src="/search-button.png" className="w-5 h-5 mr-4" />
+            <span className="font-semibold font-inter">Explore</span>
           </div>
         </button>
         <button
-          className={(!search && active) + " w-1/2 py-3 px-16"}
-          onClick={ () => handleClick(false) }
+          className="group absolute right-0 w-1/2 py-3 px-16"
+          onClick={() => handleClick(false)}
         >
+          <div className="absolute top-0 left-0 right-0 bottom-0 group-hover:bg-white/10 rounded-xl"></div>
           <div className="flex justify-center items-center">
-            <img
-              src="/saved-button.png"
-              className="w-4 h-5 mr-4"
-            />
-            <span className="font-semibold font-inter">
-              Saved
-            </span>
+            <img src="/saved-button.png" className="w-4 h-5 mr-4" />
+            <span className="font-semibold font-inter">Saved</span>
           </div>
         </button>
       </div>
     </div>
-  );
+  )
 }
